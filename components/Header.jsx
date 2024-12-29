fdf97fce753a1eda2ff14331ff05f16ef28997cc
jf <linzhoufang@gmail.com>
2024-12-29 00:52:07 -0800

    header naming

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,11 +2,9 @@ import Globe from "/assets/img/Globe.svg"
 
 export default function Header () {
     return (
-        <>
-            <div className="header-container">
-                <img src={Globe} alt="Globe" />
-                <p className="header-title">my travel journal.</p>
-            </div>
-        </>
+        <header>
+            <img src={Globe} alt="Globe" />
+            <p className="header-title">my travel journal.</p>
+        </header>
     )
 }
