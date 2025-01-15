38118d7d5672ea8c493a6586b72ae71518f401df
jf <zhoufang29@gmail.com>
2025-01-15 01:12:47 -0800

    process non string props and parse various travelDurations

--- App.jsx
+++ App.jsx
@@ -14,6 +14,8 @@ export default function App () {
                 areaName="Surrey, BC"
                 locationMapLink="https://www.google.com/maps/place/Surrey+Central/@49.1894304,-122.8498537,16z/data=!3m1!4b1!4m6!3m5!1s0x5485d82b29464427:0x283cff04830abc17!8m2!3d49.1894304!4d-122.8483981!16s%2Fm%2F04zg7rc?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D"
                 travelDate="23 Nov, 2024 - 24 Nov, 2024"
+                travelMonth="Nov 2024"
+                travelDuration={2}
                 infoContent="Come and see the christmas tree lighting! Lots of people, rides, concerts, and treats to eat! And of course, the beautiful christmas tree!"
             />
             <Entry 
@@ -22,6 +24,8 @@ export default function App () {
                 areaName="Richmond, BC"
                 locationMapLink="https://www.google.com/maps/place/Thrangu+Monastery/@49.1541312,-123.0932331,17z/data=!3m1!4b1!4m6!3m5!1s0x5485d76de5c1a127:0x3bdcddadb9e2b811!8m2!3d49.1541312!4d-123.0906582!16s%2Fm%2F0cmbbxp?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D"
                 travelDate="17 Nov, 2024"
+                travelMonth="Nov 2024"
+                travelDuration={1}
                 infoContent="Cool place with a beautiful buddha."
             />
             <Entry 
@@ -30,6 +34,8 @@ export default function App () {
                 areaName="Seattle, WA"
                 locationMapLink="https://www.google.com/maps/place/University+of+Washington,+Seattle,+WA,+USA/@47.6543747,-122.3134106,15z/data=!3m1!4b1!4m6!3m5!1s0x54901492f492ee53:0xc49de620ee4a0b02!8m2!3d47.6516448!4d-122.3081762!16s%2Fg%2F11vr9rvds7?entry=ttu&g_ep=EgoyMDI0MTIxMS4wIKXMDSoASAFQAw%3D%3D"
                 travelDate="13 Oct, 2024 - 14 Oct, 2024"
+                travelMonth="Oct 2024"
+                travelDuration={2}
                 infoContent="A beautiful place of marvel that reminds travellers of the Renaissance, with stunning buildings shaped like castles, and libraries shaped like churches. GO HUSKIES!"
             />
             {/* <Entry 
--- components/Entry.jsx
+++ components/Entry.jsx
@@ -1,6 +1,46 @@
 import Marker from "/assets/img/Marker.svg"
 
+function parseDuration(travelDuration) {
+    if (!travelDuration || isNaN(travelDuration)) {
+        return ""; 
+    }
+    
+    let durationStr = "";
+    if (travelDuration == 1) {
+        durationStr = " (1 day)";
+    } else if (travelDuration < 6) {
+        durationStr = " (" + travelDuration + " days" + ")";
+    } else if (travelDuration < 28) {
+        let duration = Math.round(travelDuration / 7);
+        durationStr = " (" + duration;
+        if (duration > 1) {
+            durationStr += " weeks" + ")";
+        } else {
+            durationStr += " week" + ")";
+        }
+    } else if (travelDuration < 365) {
+        let duration = Math.round(travelDuration / 30);
+        durationStr = " (" + duration;
+        if (duration > 1) {
+            durationStr += " months" + ")";
+        } else {
+            durationStr += " month" + ")";
+        }
+    } else {
+        let duration = Math.round(travelDuration / 365);
+        durationStr = " (" + duration;
+        if (duration > 1) {
+            durationStr += " years" + ")";
+        } else {
+            durationStr += " year" + ")";
+        }
+    }
+    return durationStr;
+}
+
 export default function Entry (props) {
+    let durationStr = parseDuration(props.travelDuration);
+    
     return (
         <article className="journal-entry">
                 <div className="image-container">
@@ -20,7 +60,7 @@ export default function Entry (props) {
                     </h1>
 
                     <div className="info-date">
-                        {props.travelDate}
+                        {props.travelMonth}{durationStr}
                     </div>
                     
                     <div className="info-content">
